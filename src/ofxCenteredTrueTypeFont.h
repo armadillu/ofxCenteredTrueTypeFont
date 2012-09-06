175ff83b31e611d51d2e74cbe70408f7bbe25a32
Oriol Ferrer Mesià <oriol@uri.cat>
2012-09-06 14:06:29 +0200

    need to stay into integer coords

--- src/ofxCenteredTrueTypeFont.h
+++ src/ofxCenteredTrueTypeFont.h
@@ -8,7 +8,7 @@ class ofxCenteredTrueTypeFont : public ofTrueTypeFont{
 
 	ofVec2f getOffset( string s ){
 		ofRectangle r = getStringBoundingBox(s, 0, 0);
-		return ofVec2f( -r.x - r.width * 0.5f, -r.y - r.height * 0.5f );
+		return ofVec2f( floor(-r.x - r.width * 0.5f), floor(-r.y - r.height * 0.5f) );
 	}
 	
 	void drawStringCentered(string s, float x, float y, bool asVector = false){
@@ -19,12 +19,12 @@ class ofxCenteredTrueTypeFont : public ofTrueTypeFont{
 			drawStringAsShapes(s, x + offset.x, y + offset.y);		
 	}
 	
-	void drawCenteredBoundingBox(string s, float x, float y, float offset = 0){
+	void drawCenteredBoundingBox(string s, float x, float y, float padding = 0){
 		ofRectangle r = getStringBoundingBox(s, 0, 0);
-		r.x -= offset;
-		r.y -= offset;
-		r.width += 2.0f * offset;
-		r.height += 2.0f * offset;
+		r.x -= padding;
+		r.y -= padding;
+		r.width += 2.0f * padding;
+		r.height += 2.0f * padding;
 		ofNoFill();
 		ofSetRectMode(OF_RECTMODE_CENTER);
 		ofRect( x, y, r.width, r.height);
